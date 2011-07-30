cb0572774cedeb22d80b0f97bf4be9304eca34ce
Bob Zhome <BobZhome@cheerful.com>
2011-07-29 20:07:44 -0400

    cleaning up

--- products/cyanogen_ascend.mk
+++ products/cyanogen_ascend.mk
@@ -22,7 +22,7 @@ PRODUCT_BRAND := google
 PRODUCT_DEVICE := ascend
 PRODUCT_MODEL := M860
 PRODUCT_MANUFACTURER := Huawei
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=sojus BUILD_ID=GRJ22 BUILD_DISPLAY_ID=GRJ22 BUILD_FINGERPRINT=google/sojus/crespo4g:2.3.4/GRJ22/121341:user/release-keys PRIVATE_BUILD_DESC="sojus-user 2.3.4 GRJ22 121341 release-keys" BUILD_NUMBER=121341
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=blade BUILD_ID=GRJ22 BUILD_DISPLAY_ID=GRJ90 BUILD_FINGERPRINT=google/passion/passion:2.3.5/GRJ22/121341:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.3.5 GRJ22 121341 release-keys"
 
 #PRODUCT_SPECIFIC_DEFINES += TARGET_PRELINKER_MAP=$(TOP)/vendor/cyanogen/prelink-linux-arm-ascend.map
 
@@ -46,23 +46,7 @@ endif
 # Copy legend specific prebuilt files
 #
 PRODUCT_COPY_FILES += \
-    vendor/cyanogen/prebuilt/mdpi/media/bootanimation.zip:system/media/bootanimation.zip
-PRODUCT_COPY_FILES += \
-    vendor/cyanogen/prebuilt/mdpi/media/follow.ogg:system/media/audio/ringtones/follow.ogg
-PRODUCT_COPY_FILES += \
-    vendor/cyanogen/prebuilt/mdpi/media/somebodys.ogg:system/media/audio/ringtones/somebodys.ogg
-
-# Lights and graphics
-PRODUCT_COPY_FILES += \
-    vendor/huawei/ascend/proprietary/lib/hw/gralloc.msm7k.so:system/lib/hw/gralloc.msm7k.so
-PRODUCT_COPY_FILES += \
-    vendor/huawei/ascend/proprietary/lib/hw/lights.msm7k.so:system/lib/hw/lights.msm7k.so
-PRODUCT_COPY_FILES += \
-    vendor/huawei/ascend/proprietary/lib/libui.so:system/lib/libui.so
-
-# Proprietary RIL related
-PRODUCT_COPY_FILES += \
-    vendor/huawei/ascend/proprietary/lib/libril.so:system/lib/libril.so
-PRODUCT_COPY_FILES += \
-    vendor/huawei/ascend/proprietary/lib/libwpa_client.so:system/lib/libwpa_client.so
+    vendor/cyanogen/prebuilt/mdpi/media/bootanimation.zip:system/media/bootanimation.zip \
+    vendor/cyanogen/prebuilt/mdpi/media/follow.ogg:system/media/audio/ringtones/follow.ogg \
+    vendor/cyanogen/prebuilt/mdpi/media/somebodys.ogg:system/media/audio/ringtones/somebodys.ogg \
 
