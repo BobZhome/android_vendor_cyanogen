82dd2917beb412001874c84a74edf6098a46defb
BobZhome <BobZhome@Cheerful.com>
2011-10-29 09:50:16 -0400

    cm did not like slider part 2

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,11 +1,10 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/cyanogen_ascend.mk \
-    $(LOCAL_DIR)/cyanogen_slider.mk \
-    $(LOCAL_DIR)/cyanogen_slider_sprint.mk \
-    $(LOCAL_DIR)/cyanogen_slider_virgin.mk \
     $(LOCAL_DIR)/cyanogen_thunderc.mk \
     $(LOCAL_DIR)/cyanogen_thunderc_cricket.mk \
     $(LOCAL_DIR)/cyanogen_thunderc_metropcs.mk \
+    $(LOCAL_DIR)/cyanogen_thunderc_slider_sprint.mk \
+    $(LOCAL_DIR)/cyanogen_thunderc_slider_virgin.mk \
     $(LOCAL_DIR)/cyanogen_thunderc_sprint.mk \
     $(LOCAL_DIR)/cyanogen_thunderc_usc.mk \
     $(LOCAL_DIR)/cyanogen_thunderc_verizon.mk \
--- products/cyanogen_slider_sprint.mk
+++
@@ -1,3 +0,0 @@
-SUB_MODEL := LS700
-
-$(call inherit-product, vendor/cyanogen/products/cyanogen_slider.mk)
